1932ed194a244053c4165b0dc75d8e2902ea57ab
kaufman79 <kaufman79@gmail.com>
2024-06-12 08:22:06 -0400

    add lined papers and dash lined papers (as separate .py files)

---
+++ lined_papers.py
@@ -0,0 +1,41 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit="mm",format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    # set the header:
+    pdf.add_page()
+    pdf.set_font(family="Times",style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
+             ln=1, border=0)
+    # set lines
+    for y in range(20, 298, 10):
+        pdf.line(x1=10, y1=y, x2=200, y2=y) # x1 & y1 are starting coordinates of line, 2s: ending
+
+
+    # set the footer
+    pdf.ln(260) # x mm of break lines
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R",
+             ln=1, border=0)
+
+    # make additional pages without a header
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+        # set lines
+        for y in range(20, 298, 10):
+            pdf.line(x1=10, y1=y, x2=200, y2=y)
+        pdf.ln(272)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R",
+                 ln=1, border=0)
+
+
+pdf.output("output_lined.pdf")
